0202cdaa9818272c1d9e1d4b1bc36e37fdef3003
Piyush Kumar <pgh5566@gmail.com>
2023-12-29 11:10:23 +0530

    Initializing project

---
+++ src/main/java/com/example/dao/HelloDao.java
@@ -0,0 +1,10 @@
+package com.example.dao;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+public class HelloDao {
+
+}
---
+++ src/main/java/com/example/entity/HelloEntity.java
@@ -0,0 +1,8 @@
+package com.example.entity;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+public class HelloEntity {
+}
